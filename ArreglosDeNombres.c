d86738b2d9537e2ad67306e4be2888484854ff06
ana-jo-nava01 <anajonava14@gmail.com>
2026-04-21 09:52:57 -0300

    implementada BuscaNombre

--- ArreglosDeNombres.c
+++ ArreglosDeNombres.c
@@ -4,13 +4,14 @@
 #define MAX 5
 
 void MostrarPersonas(char *V[]);
+void BuscarNombre(int n, char *V[]);
 
 int main() {
     char *V[MAX];
     char buff[50];
 
     for(int i = 0; i < MAX; i++) {
-        printf("Ingrese el nombre de la persona %d: ", (i + 1));
+        printf("Ingrese el nombre de la persona %d: ", i);
         scanf("%s", buff);
 
         int tamNombre = strlen(buff);
@@ -19,13 +20,30 @@ int main() {
     }
     
     MostrarPersonas(V);
+    
+    int n;
+    printf("Ingrese el indice que desea buscar: ");
+    scanf("%d", &n);
+    BuscarNombre(n, V);
 
     return 0;
 }
 
 void MostrarPersonas(char *V[]) {
     for(int i = 0; i < MAX; i++) {
-        printf("Nombre %d: ", (i + 1));
+        printf("Nombre %d: ", i);
         puts(V[i]);
+    }
+}
+
+void BuscarNombre(int n, char *V[]) {
+    if(n >= 0 && n < 5) {
+        for(int i = 0; i < 5; i++) {
+            if(i == n) {
+                puts(V[i]);
+            }
+        }
+    } else {
+        printf("El valor no pertenece al vector. \n");
     }
 }
